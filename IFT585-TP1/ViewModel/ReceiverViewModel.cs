e614fb29b18123b04a95544e14f18b98cce25583
Bruno Cadorette <brunocadorette@outlook.com>
2015-05-21 15:20:12 -0400

    Add receiver

---
+++ IFT585-TP1/ViewModel/ReceiverViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UDPClient;
+
+namespace IFT585_TP1.ViewModel
+{
+    class ReceiverViewModel : BindableBase
+    {
+        public ObservableCollection<ProgressViewModel> DownloadingFiles { get; set; }
+
+        private class Udp : IUDP
+        {
+            public int FileSize
+            {
+                get
+                {
+                    return 100;
+                }
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public EventHandler<AckEventArgs> PacketReceived
+            {
+                get;
+                set;
+            }
+
+            public EventHandler<AckEventArgs> Resended
+            {
+                get;
+                set;
+            }
+        }
+
+        public ICommand Listen { get; private set; }
+
+        private int port;
+        public int Port
+        {
+            get
+            {
+                return port;
+            }
+            set
+            {
+                SetProperty(ref port, value);
+            }
+        }
+
+        private Listener listener;
+
+        public ReceiverViewModel()
+        {
+            Listen = new ActionCommand(() => { 
+                listener = new Listener(port);
+                listener.StartListening();
+            });
+            DownloadingFiles = new ObservableCollection<ProgressViewModel>()
+            {
+                new ProgressViewModel(new Udp()),
+                new ProgressViewModel(new Udp()),
+                new ProgressViewModel(new Udp())
+            };
+        }
+
+    }
+}
