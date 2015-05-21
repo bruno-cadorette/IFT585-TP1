a37a9fb1dd055cd440079f50d797f7159544982a
Bruno Cadorette <brunocadorette@outlook.com>
2015-05-21 13:50:05 -0400

    ProgressBar view model

---
+++ IFT585-TP1/ViewModel/ProgressViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UDPClient;
+
+namespace IFT585_TP1
+{
+    class ProgressViewModel : BindableBase
+    {
+        private int percent = 0;
+        public int ProgressBarPercent
+        {
+            get
+            {
+                return percent;
+            }
+            set
+            {
+                SetProperty(ref percent, value);
+            }
+        }
+
+        public int FileSize
+        {
+            get
+            {
+                return sender.FileSize;
+            }
+        }
+
+        private UDPClientSender sender;
+
+        public ProgressViewModel(UDPClientSender sender)
+        {
+            this.sender = sender;
+            this.sender.ACKReceived += (o, e) => ProgressBarPercent++;
+        }
+    }
+}
